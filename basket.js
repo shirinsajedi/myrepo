98e295e673f5c3af4c7fca1636de610a23810314
t.sajedi <taha.developer1997@gmail.com>
2021-09-30 23:53:54 +0330

    test

--- basket.js
+++ basket.js
@@ -2,6 +2,8 @@ localStorage.setItem("1","Redmi note 10 pro");
 document.querySelector("1").addEventListener("onclick",goTobasket);
 
  function goTobasket(){
+
+    //  test push
     alert("به سبد خرید اضافه شد ");
 }
 
