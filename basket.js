e281bdfb192dffb4a0fe5c8fcfe37ed1be02a6c9
t.sajedi <taha.developer1997@gmail.com>
2021-09-30 12:51:38 +0330

    add js

---
+++ basket.js
@@ -0,0 +1,7 @@
+localStorage.setItem("1","Redmi note 10 pro");
+document.querySelector("1").addEventListener("onclick",goTobasket);
+
+ function goTobasket(){
+    alert("به سبد خرید اضافه شد ");
+}
+
---
+++ index.js
@@ -0,0 +1,13 @@
+/*document.querySelector(".buy").addEventListener("click",myFunction);
+function myFunction(){
+    window.open
+}
+ const img = document.createElement("img");
+img.className = "card-img-top" 
+img.src = "images/images (5).jpg"
+    img.appendChild(document.createTextNode("Redmi note 10 pro  m-auto  text=danger"));
+
+
+localStorage.setItem("img","images/images (5).jpg");
+const ha = localStorage.getItem("img");
+console.log(ha);*/
