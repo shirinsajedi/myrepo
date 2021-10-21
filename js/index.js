9a3de8b457874744af4cb641534f0dc17d7f4823
t.sajedi <taha.developer1997@gmail.com>
2021-10-21 13:12:41 +0330

    add user page and back to top

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
-/*document.querySelector(".buy").addEventListener("click",myFunction);
-function myFunction(){
-    window.open
-}
- const img = document.createElement("img");
-img.className = "card-img-top" 
-img.src = "images/images (5).jpg"
-    img.appendChild(document.createTextNode("Redmi note 10 pro  m-auto  text=danger"));
-
-
-localStorage.setItem("img","images/images (5).jpg");
-const ha = localStorage.getItem("img");
-console.log(ha);*/
+const totop = document.querySelector(".to-top");
+window.addEventListener("scroll", function() {
+    if (window.pageYOffset > 300) {
+        totop.classList.add("active");
+    } else(totop.classList.remove("active"))
+});
